b66d306c9f7616afbb8e6f768bf346f1ecf07ef0
picks6 <Zachary.picklesimer@gmail.com>
2022-06-02 15:53:28 -0400

    created resolvers and typedefs.js

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,92 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { Deck, Card, User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    decks: async () => {
+        //return all decks
+        return await Deck.find({});
+    },
+    deck: async(parent, {deckId}) => {
+      return await Deck.findOne({ _id: deckId });
+    },
+    deckTitle: async(parent,{deckTitle}) =>{
+      return await Deck.findOne({title: deckTitle})
+    },
+    card: async (parent, { deck }) => {
+      return await Card.find({deck: deck});
+    },
+    cards: async (parent, args) => {
+      return Card.find({});
+    },
+    //find the user by ID, populate decks and cards
+    user: async (parent, args, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id).populate({
+          path: 'decks',
+          populate: 'cards'
+        });
+        return user;
+      }
+    },
+  },
+  Mutation: {
+    //this is creating a user from the sign up page
+  addUser: async (parent, args) => {
+    const user = await User.create(args);
+    const token = signToken(user);
+
+    return { token, user };
+  },
+  //addDeck
+  addDeck: async (parent, {title, category, description}) => {
+      const deck = await Deck.create({title, category, description});
+      return deck;
+  },
+  // addCard
+  addCard: async (parent, {sideA, sideB, deck}) => {
+      const card = await Card.create({sideA, sideB, deck});
+      return card;
+  },
+  // Update User
+  updateUser: async (parent, args, context) => {
+    if (context.user) {
+      return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+    }
+    throw new AuthenticationError('Not logged in');
+  },
+  //Update Deck 
+  updateDeck: async (parent, args, context) => {
+      if (context.deck) {
+          return await Deck.findByIdAndUpdate(context.deck._id, args, {new: true});
+      }
+  },
+  //Update Card
+  updateCard: async (parent, args, context) => {
+      if (context.deck) {
+          return await Card.findByIdAndUpdate(context.card._id, args, {new: true});
+      }
+  },
+  //Login check is by email, password requirement is 8 characters
+  login: async (parent, { username, password }) => {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      throw new AuthenticationError('Incorrect credentials');
+    }
+
+    const correctPw = await user.isCorrectPassword(password);
+
+    if (!correctPw) {
+      throw new AuthenticationError('Incorrect credentials');
+    }
+
+    const token = signToken(user);
+
+    return { token, user };
+  }
+}
+};
+
+module.exports = resolvers;
