3fff49f1ff338f72e2b374e58d2496b08846e4dd
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-03 12:38:05 -0400

    user auth created

---
+++ client/src/utils/auth.js
@@ -0,0 +1,36 @@
+import decode from 'jwt-decode';
+
+class Auth {
+  login(token) { // save token to local storage
+    localStorage.setItem('id_token', token);
+    // window.location.assign('/'); // reload page
+  }
+
+  getToken() {
+    return localStorage.getItem('id_token');
+  }
+
+  getUser() {
+    return decode(this.getToken());
+  }
+
+  isTokenExpired(token) {
+    try {
+      const decodedToken = decode(token);
+      // check if expiration time has passed
+      decodedToken.exp < Date.now()/1000 ? true : false;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  isLoggedIn() {
+    const token = this.getToken();
+    return token && !this.isTokenExpired(token);
+  }
+
+  logout() {
+    localStorage.removeItem('id_token');
+    // window.location.assign('/'); // reload page
+  }
+}
