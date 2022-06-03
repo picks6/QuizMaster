26ac49967207eb2728909cfe712a94d65589155a
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-03 16:14:11 -0400

    email added to variables

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -86,8 +86,8 @@ export const UPDATE_DECK = gql`
     }
   }`;
 export const LOGIN_USER = gql`
-  mutation Login($username: String!, $password: String!) {
-    login(username: $username, password: $password) {
+  mutation Login($username: String, $email: String $password: String!) {
+    login(username: $username, email: $email, password: $password) {
       token
       user {
         _id
