e0931893ba8d9f7f869a5d597053d19f7502fccc
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-02 21:42:34 -0400

    gql queries matching typeDefs created

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,2 +1,99 @@
 import { gql } from '@apollo/client';
-
+export const GET_DECKS = gql`
+  query getDecks {
+    decks {
+      _id
+      title
+      category
+      description
+      creator
+      date_created
+      cards {
+        _id
+        sideA
+        sideB
+        deckTitle
+        deck
+      }
+    }
+  }`;
+export const QUERY_DECK = gql`
+  query queryDeck($deckId: ID) {
+    deck(deckId: $deckId) {
+      _id
+      title
+      category
+      description
+      creator
+      date_created
+      cards {
+        _id
+        sideA
+        sideB
+        deckTitle
+        deck
+      }
+    }
+  }`;
+export const QUERY_TITLE = gql`
+  query DeckTitle($deckTitle: String) {
+    deckTitle(deckTitle: $deckTitle) {
+      _id
+      title
+      category
+      description
+      creator
+      date_created
+      cards {
+        _id
+        sideA
+        sideB
+        deckTitle
+        deck
+      }
+    }
+  }`;
+export const QUERY_CARD = gql`
+  query queryCard($deck: ID) {
+    card(deck: $deck) {
+      _id
+      sideA
+      sideB
+      deckTitle
+      deck
+    }
+  }`;
+export const GET_CARDS = gql`
+  query getCards {
+    cards {
+      _id
+      sideA
+      sideB
+      deckTitle
+      deck
+    }
+  }`;
+export const QUERY_USER = gql`
+  query queryUser {
+    user {
+      _id
+      username
+      email
+      password
+      decks {
+        _id
+        title
+        category
+        description
+        creator
+        date_created
+        cards {
+          _id
+          sideA
+          sideB
+          deckTitle
+          deck
+        }
+      }
+    }
+  }`;
