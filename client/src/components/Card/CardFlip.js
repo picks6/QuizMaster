d35df2d2b377ea6175b242dff2a9d21ec21f974c
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-04 11:29:51 -0400

    QUERY_CARD connected and tested

--- client/src/components/Card/CardFlip.js
+++ client/src/components/Card/CardFlip.js
@@ -2,29 +2,35 @@ import React, {useState} from 'react'
 import ReactCardFlip from 'react-card-flip';
 import { Card, Button, Header, Grid } from 'semantic-ui-react'
 
+import { useQuery } from '@apollo/client';
+import { GET_CARDS, QUERY_CARD } from '../../utils/queries';
 
 const CardFlip = () => {
+
+  const { loading, error, data } = useQuery(QUERY_CARD, { variables: { cardId: '629a4823d329d4ec1515c3b6'}});
+  console.log(data);
   const[isFlipped, setIsFlipped] = useState(false);
   
   const handleClick = () =>{
     setIsFlipped(!isFlipped);
   }
-  
+  if (loading) return <div>Loading</div>; 
+  if (error) return `Error! ${error.message}`;
   return (
     <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
     <Grid.Column style={{ maxWidth: 450 }}>
         <Header as='h2' color='teal' textAlign='center'>deckTitle</Header>
     <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
         <Card>  
-        <Card.Content>
-          sideA
+          <Card.Content>
+            {data.card.sideA}
           </Card.Content>
           <Button color='teal' onClick={handleClick}>Click to flip</Button>
         </Card>
 
         <Card>
-        <Card.Content>
-          sideB
+          <Card.Content>
+            {data.card.sideB}
           </Card.Content>
           <Button color='teal' onClick={handleClick}>Click to flip</Button>
         </Card>
