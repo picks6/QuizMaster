e3118aed486660724488e617f87555267da652e8
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-05 14:33:50 -0400

    query removed, props deconstructed

--- client/src/components/quizmaster/Deck.js
+++ client/src/components/quizmaster/Deck.js
@@ -1,25 +1,13 @@
 import React, {useState} from 'react';
+import { Card, Button } from 'semantic-ui-react';
+// import CardFlip from '../quizmaster/CardFlip';
 
-import CardFlip from '../quizmaster/CardFlip';
-
-import { useQuery } from '@apollo/client';
-import { QUERY_DECK } from '../../utils/queries';
-
-const Deck = () => { 
-  // Deck ID needs to be passed to component for query to function
-
-  const { loading, error, data } = useQuery(QUERY_DECK, {
-    variables: {}
-  });
-  console.log('GET_CARDS:', data);
-  const cards = data.cards
-
-  if (loading) return <div>Loading</div>; 
-  if (error) return `Error! ${error.message}`;
+const Deck = ({ deck }) => { 
+  // Deck ID must be passed
 
   return (
     <div>
-      {cards.map((el) => <CardFlip sideA={el.sideA} sideB={el.sideB} />)}
+      {/* {cards.map((el) => <CardFlip sideA={el.sideA} sideB={el.sideB} />)} */}
     </div>
   )
 }
