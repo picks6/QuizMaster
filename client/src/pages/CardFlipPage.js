cd72c1008294d0eb755a81f76a46ac67b959e894
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-05 14:32:12 -0400

    props deconstructed for clarity

--- client/src/pages/CardFlipPage.js
+++ client/src/pages/CardFlipPage.js
@@ -3,10 +3,10 @@ import CardFlip from "../components/quizmaster/CardFlip";
 import { useQuery } from "@apollo/client";
 import { QUERY_CARD } from "../utils/queries";
 
-function CardFlipPage(props) {
+function CardFlipPage({ deckId, cardId }) {
   // Card ID and Deck ID must be passed.
   const { loading, error, data } = useQuery(QUERY_CARD, {
-    // variables: { deckId: props.deckId, cardId: props.cardId },
+    // variables: { deckId, cardId },
     variables: { deckId: "629c07565cac8a2aa7e11d62", cardId: "629c07565cac8a2aa7e11d6d" } // for testing
   }); // returns single card
   // Expect data: {
