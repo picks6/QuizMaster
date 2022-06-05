fe85af1b97a31f62000038082ca155868548f538
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-05 14:36:36 -0400

    QUERY_DECK connected, tested, passed to component

--- client/src/pages/DeckPage.js
+++ client/src/pages/DeckPage.js
@@ -1,9 +1,23 @@
 import Deck from "../components/quizmaster/Deck";
 
-function DeckPage() {
+import { useQuery } from '@apollo/client';
+import { QUERY_DECK } from '../utils/queries';
+
+function DeckPage({ deckId }) { // expect deckId to be passed as a prop
+
+  const { loading, error, data } = useQuery(QUERY_DECK, {
+    // variables: { deckId }
+    variables: { deckId: ""} // for testing
+  });
+  if (loading) return <div>Loading</div>;
+  if (error) return <div>Error! {`${error.message}`}</div>;
+
+  console.log('QUERY_DECK:', data.deck);
+  const deck = data.deck
+
   return (
     <section>
-      <Deck />
+      <Deck deck={deck} />
     </section>
   );
 }
