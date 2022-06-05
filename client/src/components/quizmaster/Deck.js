b32abc7957eb33af0c127ffce0bfb2eb1630b146
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-05 11:06:38 -0400

    GET_CARDS removed

--- client/src/components/quizmaster/Deck.js
+++ client/src/components/quizmaster/Deck.js
@@ -3,12 +3,17 @@ import React, {useState} from 'react';
 import CardFlip from '../quizmaster/CardFlip';
 
 import { useQuery } from '@apollo/client';
-import { GET_CARDS, QUERY_CARD } from '../../utils/queries';
+import { QUERY_DECK } from '../../utils/queries';
 
-const Deck = () => {
-  const { loading, error, data } = useQuery(GET_CARDS);
+const Deck = () => { 
+  // Deck ID needs to be passed to component for query to function
+
+  const { loading, error, data } = useQuery(QUERY_DECK, {
+    variables: {}
+  });
   console.log('GET_CARDS:', data);
   const cards = data.cards
+
   if (loading) return <div>Loading</div>; 
   if (error) return `Error! ${error.message}`;
 
