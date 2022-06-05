2918cdb504a6ccb78b1796e97c34d0e9cbc6da5b
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-05 13:37:03 -0400

    argument order updated, IDs now usually required

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -33,18 +33,18 @@ const typeDefs = gql`
 
   type Query {
     decks: [Deck]
-    deck(deckId: ID): Deck
-    deckTitle(deckTitle: String): Deck
-    card(deck: ID, cardId: ID): Deck
+    deck(deckId: ID!): Deck
+    deckTitle(deckTitle: String!): Deck
+    card(deckId: ID!, cardId: ID!): Deck
     user: User
   }
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     addDeck(title: String!, category: String!, description: String): Deck
-    addCard(sideA: String!, sideB: String!, deck: ID!): Deck
-    updateCard(deck: ID!, cardId: ID!, sideA: String!, sideB: String!): Deck
-    updateUser(username: String, email: String, password: String, deck: ID): User
-    updateDeck(title: String, category: String, description: String, cardId: ID): Deck
+    addCard(deckId: ID!, sideA: String!, sideB: String!): Deck
+    updateCard(deckId: ID!, cardId: ID!, sideA: String!, sideB: String!): Deck
+    updateUser(username: String, email: String, password: String, deckId: ID): User
+    updateDeck(deckId: ID!, title: String, category: String, description: String): Deck
     login( username: String, email: String, password: String!): Auth
   }
 `;
