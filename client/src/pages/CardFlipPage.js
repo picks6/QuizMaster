cb0f8afb94af26d5324af9e76a9e8d1e8aa2cf16
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-05 14:06:09 -0400

    QUERY_CARD connected and tested

--- client/src/pages/CardFlipPage.js
+++ client/src/pages/CardFlipPage.js
@@ -1,9 +1,27 @@
 import CardFlip from "../components/quizmaster/CardFlip";
 
-function CardFlipPage() {
+import { useQuery } from "@apollo/client";
+import { QUERY_CARD } from "../utils/queries";
+
+function CardFlipPage(props) {
+  // Card ID and Deck ID must be passed.
+  const { loading, error, data } = useQuery(QUERY_CARD, {
+    variables: { deckId: props.deckId, cardId: props.cardId },
+    // variables: { deckId: "", cardId: "" } // for testing
+  }); // returns single card
+  // Expect data: {
+  //   card: {
+  //     cards: [{ sideA, sideB, deck }]
+  //   }
+  // }
+  const card = data.card.cards[0];
+
+  if (loading) return <div>Loading</div>;
+  if (error) return <div>Error! {`${error.message}`}</div>;
+
   return (
     <section>
-      <CardFlip />
+      <CardFlip card={card} />
     </section>
   );
 }
