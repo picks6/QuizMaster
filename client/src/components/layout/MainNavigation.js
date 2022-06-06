58c337a4106e4a81fc9b710befa656c8b45a56f1
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-06 14:07:07 -0400

    extract buttons as components, render conditionally

--- client/src/components/layout/MainNavigation.js
+++ client/src/components/layout/MainNavigation.js
@@ -1,29 +1,67 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Menu, Button, Icon, Input } from "semantic-ui-react";
 
-import { Menu, Button, Icon } from "semantic-ui-react";
+import Auth from "../../utils/auth";
+
 import classes from "./MainNavigation.module.css";
 
-const MainNavigation = () => (
-  <Menu inverted>
-    <Menu.Item>
-      <Button as={Link} to="/dashboard" inverted color="olive">
-        Dashboard
-      </Button>
-    </Menu.Item>
-    <Menu.Item position="right">
-      <Button as={Link} to="/login" inverted color="orange">
-        Log in
-      </Button>
-    </Menu.Item>
-    <Menu.Item>
-      <Button as={Link} to="/signup" color="teal">
-        Sign Up
-      </Button>
-    </Menu.Item>
-    <Menu.Item>
-      <Icon name="log out" color="red"></Icon>
-    </Menu.Item>
-  </Menu>
+const DashboardButton = () => (
+  <Menu.Item>
+    <Button as={Link} to="/dashboard" inverted color="olive">
+      Dashboard
+    </Button>
+  </Menu.Item>
 );
+const SearchBar = () => (
+  <Menu.Item position="right">
+    <Input icon='search' placeholder='Search...' />
+  </Menu.Item>
+)
+const LoginButton = () => (
+  <Menu.Item>
+    <Button as={Link} to="/login" inverted color="orange">
+      Log in
+    </Button>
+  </Menu.Item>
+);
+const SignupButton = () => (
+  <Menu.Item>
+    <Button as={Link} to="/signup" color="teal">
+      Sign Up
+    </Button>
+  </Menu.Item>
+);
+const LogoutButton = (props) => (
+  <Menu.Item>
+    <Icon 
+      name="log out" 
+      color="red" 
+      onclick={Auth.logout}
+    />
+  </Menu.Item>
+);
+
+const MainNavigation = () => {
+  
+  return (
+    <Menu inverted>
+      {
+        Auth.isLoggedIn() ? (
+          <>
+            <DashboardButton />
+            <SearchBar />
+            <LogoutButton />
+          </>
+        ) : (
+          <>
+            <LoginButton />
+            <SearchBar />
+            <SignupButton />
+          </>
+        )
+      }
+    </Menu>
+  )
+};
 export default MainNavigation;
