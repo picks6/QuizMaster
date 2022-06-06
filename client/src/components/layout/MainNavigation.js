48d82ec839d084072af7d6a39f444b7d5070f2c6
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-06 10:54:56 -0400

    jsx now react function and exported, debugged

--- client/src/components/layout/MainNavigation.js
+++ client/src/components/layout/MainNavigation.js
@@ -1,26 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { Menu, Button, Icon } from "@semantic-ui-react";
+import { Menu, Button, Icon } from "semantic-ui-react";
 import classes from "./MainNavigation.module.css";
 
-<Menu inverted>
-  <Menu.Item>
-    <Button as={Link} to="/dashboard" inverted color="olive">
-      Dashboard
-    </Button>
-  </Menu.Item>
-  <Menu.Item position="right">
-    <Button as={Link} to="/login" inverted color="orange">
-      Log in
-    </Button>
-  </Menu.Item>
-  <Menu.Item>
-    <Button as={Link} to="/signup" color="teal">
-      Sign Up
-    </Button>
-  </Menu.Item>
-  <Menu.Item>
-    <Icon name="log out" color="red"></Icon>
-  </Menu.Item>
-</Menu>;
+const MainNavigation = () => (
+  <Menu inverted>
+    <Menu.Item>
+      <Button as={Link} to="/dashboard" inverted color="olive">
+        Dashboard
+      </Button>
+    </Menu.Item>
+    <Menu.Item position="right">
+      <Button as={Link} to="/login" inverted color="orange">
+        Log in
+      </Button>
+    </Menu.Item>
+    <Menu.Item>
+      <Button as={Link} to="/signup" color="teal">
+        Sign Up
+      </Button>
+    </Menu.Item>
+    <Menu.Item>
+      <Icon name="log out" color="red"></Icon>
+    </Menu.Item>
+  </Menu>
+);
+export default MainNavigation;
