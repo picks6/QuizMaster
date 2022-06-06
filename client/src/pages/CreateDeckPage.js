04c44a1803fee213a3c3d98ca02e57ff311210f9
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-06 19:10:35 -0400

    methods moved outside component

--- client/src/pages/CreateDeckPage.js
+++ client/src/pages/CreateDeckPage.js
@@ -7,6 +7,53 @@ import CardForm from '../components/quizmaster/CardForm';
 import { useMutation } from '@apollo/client';
 import { ADD_DECK, ADD_CARD } from '../utils/mutations';
 
+const handleDeckFormChange = (event) => {
+  const { name, value } = event.target;
+  setDeckFormState({ ...deckformState, [name]: value });
+};
+const handleDeckFormSubmit = async (event) => {
+  event.preventDefault();
+  try {
+    const { data } = await addDeck({variables: { ...deckformState }});
+    const newDeck = data.addDeck;
+    console.log('ADD_DECK:', newDeck);
+    setDeck(newDeck);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const handleClick = async (event) => {
+  setCardState({ editing: true })
+}
+
+const handleCardFormChange = async (event) => {
+  const { name, value } = event.target;
+  setCardFormState({ ...cardFormState, [name]: value});
+};
+const handleCardFormSubmit = async (event) => {
+  event.preventDefault();
+  console.log({ ...cardFormState, deckId: deck._id });
+  try {
+    const { data } = await addCard(
+      { variables: { ...cardFormState, deckId: deck._id } }
+    );
+    const newDeck = data.addCard;
+    console.log('ADD_CARD', newDeck);
+    setDeck(newDeck)
+    setCardState({ editing: false });
+    setCardFormState('');
+  } catch (error) {
+    console.log(error);
+  }
+};
+const handleCancel = (event) => {
+  event.preventDefault();
+  console.log('test');
+  setCardState({ editing: false });
+  setCardFormState('');
+}
+
 function CreateDeckPage() {
   const [deckformState, setDeckFormState] = useState(
     { title: '', category: '', description: '' }
@@ -20,53 +67,6 @@ function CreateDeckPage() {
   const [cardFormState, setCardFormState] = useState(
     { sideA: '', sideB: '' }
   );
-
-  const handleDeckFormChange = (event) => {
-    const { name, value } = event.target;
-    setDeckFormState({ ...deckformState, [name]: value });
-  };
-  const handleDeckFormSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const { data } = await addDeck({variables: { ...deckformState }});
-      const newDeck = data.addDeck;
-      console.log('ADD_DECK:', newDeck);
-      setDeck(newDeck);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleClick = async (event) => {
-    setCardState({ editing: true })
-  }
-
-  const handleCardFormChange = async (event) => {
-    const { name, value } = event.target;
-    setCardFormState({ ...cardFormState, [name]: value});
-  };
-  const handleCardFormSubmit = async (event) => {
-    event.preventDefault();
-    console.log({ ...cardFormState, deckId: deck._id });
-    try {
-      const { data } = await addCard(
-        { variables: { ...cardFormState, deckId: deck._id } }
-      );
-      const newDeck = data.addCard;
-      console.log('ADD_CARD', newDeck);
-      setDeck(newDeck)
-      setCardState({ editing: false });
-      setCardFormState('');
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  const handleCancel = (event) => {
-    event.preventDefault();
-    console.log('test');
-    setCardState({ editing: false });
-    setCardFormState('');
-  }
 
   if (deck === '') {
     return (
