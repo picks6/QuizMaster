07919d64dde25be23b739a87a1dcac6897758b27
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-06 20:29:05 -0400

    AuthMiddleware bug fixed

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,7 +4,7 @@ const secret = '2CAA0FE411BF1F45DE564808C5DE6BAD3EB1DD801CE2668CA3589AEF01074EF7
 const expiration = '2h';
 
 module.exports = {
-    authMiddleWare: function ({ req }) {
+    authMiddleware: function ({ req }) {
         let token = req.body.token || req.query.token || req.headers.authorization;
 
         if (req.headers.authorization) { // expect req.headers.authorization: "Bearer <tokenvalue>"
