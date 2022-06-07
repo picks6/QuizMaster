da794d59be6cd5e2abbc537e8993df41552b6e79
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-07 16:20:58 -0400

    updateUser now returns Auth

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,23 +1,6 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Deck {
-    _id: ID
-    title: String
-    category: [ID]
-    description: String
-    creator: ID
-    date_created: String
-    cards: [Card]
-  }
-
-  type Card {
-    _id: ID
-    sideA: String!
-    sideB: String!
-    deck: ID
-  }
-
   type User {
     _id: ID
     username: String
@@ -25,7 +8,6 @@ const typeDefs = gql`
     password: String
     decks: [Deck]
   }
-
   type Auth {
     token: ID!
     user: User
@@ -36,25 +18,42 @@ const typeDefs = gql`
     category: String
   }
 
+  type Deck {
+    _id: ID
+    title: String
+    category: [ID]
+    description: String
+    creator: ID
+    date_created: String
+    cards: [Card]
+  }
+  type Card {
+    _id: ID
+    sideA: String!
+    sideB: String!
+    deck: ID
+  }
+
   type Query {
-    decks: [Deck]
-    deck(deckId: ID!): Deck
-    deckTitle(deckTitle: String!): Deck
+    user: User
     categories: [Category]
     category(categoryID: ID, category: String): Category
+    decks: [Deck]
+    deck(deckId: ID!): Deck
+    deckTitle(deckTitle: String!): Deck 
     card(deckId: ID!, cardId: ID!): Deck
-    user: User
   }
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
+    addCategory(categoryID: ID!, category: String!): Category
     addDeck(title: String!, category: String!, description: String): Deck
     addCard(deckId: ID!, sideA: String!, sideB: String!): Deck
-    updateCard(deckId: ID!, cardId: ID!, sideA: String!, sideB: String!): Deck
-    updateUser(username: String, email: String, password: String, deckId: ID): User
-    updateDeck(deckId: ID!, title: String, category: String, description: String): Deck
+
+    updateUser(username: String, email: String, password: String, deckId: ID): Auth
     login( username: String, email: String!, password: String!): Auth
-    addCategory(categoryID: ID!, category: String!): Category
     updateCategory(categoryID: ID!, category: String!): Category
+    updateDeck(deckId: ID!, title: String, category: String, description: String): Deck
+    updateCard(deckId: ID!, cardId: ID!, sideA: String!, sideB: String!): Deck
   }
 `;
 
