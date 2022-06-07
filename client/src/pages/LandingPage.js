89ada2cae7d723ba9a41d7d4ca3f989a29a3f0dc
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-07 15:10:28 -0400

    card previews created

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,11 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import { Card } from "semantic-ui-react";
 
 import Landing from '../components/quizmaster/Landing';
+import CategorySelect from "../components/quizmaster/CategorySelect";
 
+import { useLazyQuery } from "@apollo/client";
+import { GET_DECKS } from "../utils/queries";
+// import gql from 'graphql-tag'
+
+const Search = ({ handleSubmit, updateSearch, search, children}) => (
+  <form onSubmit={ handleSubmit }>
+    <input
+      type='text'
+      onChange={ updateSearch }
+      value={ search }
+      placeholder='Search'
+    />
+  </form>
+);
 const LandingPage = () => {
+  const [search, setSearch] = useState('');
+  const [decks, setDecks] = useState('');
+  const [categories, setCategories] = useState('');
+  const [getDecks, {}] = useLazyQuery(GET_DECKS);
 
+  const updateSearch = (event) => {
+    const { name, value } = event.target;
+    setSearch(value)
+  }
+  const handleSubmitSearch = async (event) => {
+    event.preventDefault()
+    const query = await getDecks();
+    const decks = query.data.decks;
+    console.log(decks);
+    setDecks(decks);
+  }
+  
   return (
-    <Landing />
+    <>
+      <Search 
+        handleSubmit={handleSubmitSearch} 
+        updateSearch={updateSearch} 
+        search={search}
+      />
+      <Card.Group>
+      {
+        decks.length 
+          ? decks.map(
+              deck => (
+                <Card key={deck._id}>
+                  <Card.Content>{deck.title}</Card.Content>
+                  <Card.Content>{deck.category}</Card.Content>
+                </Card>
+              )
+            )
+          : <></>
+      }
+      </Card.Group>
+      <CategorySelect />
+    </>
   )
 };
 export default LandingPage;
