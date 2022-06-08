d4f0f9dfc3ddb4d974b0f87424cb698dc50ebf9c
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-08 11:14:44 -0400

    Category handleChange moved to CreateDeck handler

--- client/src/components/quizmaster/CreateDeck.js
+++ client/src/components/quizmaster/CreateDeck.js
@@ -2,49 +2,44 @@ import React, { useState } from 'react';
 
 import { Form, Button, Checkbox } from 'semantic-ui-react';
 
-function CreateDeck({ handleChange, handleFormSubmit, formState }) {
+function CreateDeck({ handleChange, handleFormSubmit, formState, children }) {
     
-    return (
-        <Form onSubmit={handleFormSubmit}>
-            <Form.Field>
-              <label>Title</label>
-
-              <Form.Input 
-                placeholder='Title' 
-                name='title' 
-                type='text'
-                onChange={handleChange} 
-                value={formState.title} 
-              />
-            </Form.Field>
-
-            <Form.Field>
-              <label>Category</label>
-
-              <Form.Input 
-                placeholder='Category' 
-                name='category' 
-                type='text' 
-                onChange={handleChange} 
-                value={formState.category} 
-              />
-            </Form.Field>
-
-            <Form.Field>
-              <label>Description</label>
-
-              <Form.Input 
-                placeholder='Description' 
-                name='description' 
-                type='text' 
-                onChange={handleChange} 
-                value={formState.description} 
-              />
-            </Form.Field>
-
-            <Button type='submit'>Create</Button>
-        </Form>
-    )
+  return (
+    <Form onSubmit={handleFormSubmit}>
+      <Form.Field>
+        <label>Title</label>
+        <Form.Input 
+          placeholder='Title' 
+          name='title' 
+          type='text'
+          onChange={handleChange} 
+          value={formState.title} 
+        />
+      </Form.Field>
+      <Form.Field>
+        <label>Category</label>
+        <Form.Input 
+          placeholder='Category' 
+          name='category' 
+          type='text' 
+          onChange={handleChange} 
+          value={formState.category} 
+        />
+      </Form.Field>
+      <Form.Field>
+        {/* <label>Description</label>
+        <Form.Input 
+          placeholder='Description' 
+          name='description' 
+          type='text' 
+          onChange={handleChange} 
+          value={formState.description} 
+        /> */}
+        {children}
+      </Form.Field>
+      <Button type='submit'>Create</Button>
+    </Form>
+  )
 }
 
 export default CreateDeck;
