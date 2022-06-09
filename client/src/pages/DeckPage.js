745bb1e0cfe89199ded1a31aab4dbace71111089
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-08 20:51:45 -0400

    Link to DeckPage, Category imported

--- client/src/pages/DeckPage.js
+++ client/src/pages/DeckPage.js
@@ -1,19 +1,25 @@
 import Deck from "../components/quizmaster/Deck";
-
+import { useLocation } from "react-router-dom";
 import { useQuery } from '@apollo/client';
-import { QUERY_DECK } from '../utils/queries';
+import { GET_DECKS } from '../utils/queries';
 
 function DeckPage({ deckId }) { // expect deckId to be passed as a prop
+  const location = useLocation();
+  console.log('location:', location);
+  const { from } = location.state;
 
-  const { loading, error, data } = useQuery(QUERY_DECK, {
-    // variables: { deckId }
-    variables: { deckId: ""} // for testing
-  });
-  if (loading) return <div>Loading</div>;
-  if (error) return <div>Error! {`${error.message}`}</div>;
+  // console.log('from:', from);
+  // const { loading, error, data } = useQuery(GET_DECKS, 
+  //   {
+  //   // variables: { deckId }
+  //   variables: { deckId: ""} // for testing
+  // }
+  // );
+  // if (loading) return <div>Loading</div>;
+  // if (error) return <div>Error! {`${error.message}`}</div>;
 
-  console.log('QUERY_DECK:', data.deck);
-  const deck = data.deck
+  console.log('QUERY_DECK:', data.decks);
+  const deck = data.decks
 
   return (
     <section>
