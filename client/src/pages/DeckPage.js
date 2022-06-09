d9cccb3801b6d1a8bcae9300372966f961317f9e
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-08 21:26:55 -0400

    location.state properly desctructured

--- client/src/pages/DeckPage.js
+++ client/src/pages/DeckPage.js
@@ -3,10 +3,10 @@ import { useLocation } from "react-router-dom";
 import { useQuery } from '@apollo/client';
 import { GET_DECKS } from '../utils/queries';
 
-function DeckPage({ deckId }) { // expect deckId to be passed as a prop
+function DeckPage() { // expect deckId to be passed as a prop
   const location = useLocation();
   console.log('location:', location);
-  const { from } = location.state;
+  const deck = location.state;
 
   // console.log('from:', from);
   // const { loading, error, data } = useQuery(GET_DECKS, 
@@ -18,8 +18,8 @@ function DeckPage({ deckId }) { // expect deckId to be passed as a prop
   // if (loading) return <div>Loading</div>;
   // if (error) return <div>Error! {`${error.message}`}</div>;
 
-  console.log('QUERY_DECK:', data.decks);
-  const deck = data.decks
+  console.log('DECK:', deck);
+  // const deck = data.decks
 
   return (
     <section>
