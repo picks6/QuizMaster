645b75721e5deee964d960a031be29186baa6880
Richard Murphy <richinatl@gmail.com>
2022-06-11 19:42:05 -0400

    several styling changes for forms and cards

--- client/src/pages/CardFlipPage.js
+++ client/src/pages/CardFlipPage.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
-import Slider from 'react-touch-drag-slider';
-import { useLocation, useParams } from 'react-router-dom';
-import styled, { createGlobalStyle, css } from 'styled-components';
+import React, { useState } from "react";
+import Slider from "react-touch-drag-slider";
+import { useLocation, useParams } from "react-router-dom";
+import styled, { createGlobalStyle, css } from "styled-components";
 
-import CardFlip from '../components/quizmaster/CardFlip';
-import { useQuery } from '@apollo/client';
-import { QUERY_CARD, QUERY_DECK } from '../utils/queries';
+import CardFlip from "../components/quizmaster/CardFlip";
+import { useQuery } from "@apollo/client";
+import { QUERY_CARD, QUERY_DECK } from "../utils/queries";
+import FlipWrapper from "../components/ui/FlipWrapper";
 
 // define some basic styles
 const GlobalStyles = createGlobalStyle`
@@ -19,92 +20,94 @@ const GlobalStyles = createGlobalStyle`
 `;
 // The slider will fit any size container, lets go full screen...
 const AppStyles = styled.main`
-	height: 100vh;
-	width: 100vw;
+  height: 100vh;
+  width: 100vw;
 `;
 
 const Button = styled.button`
-	font-size: 2rem;
-	z-index: 10;
-	position: fixed;
-	top: 50%;
-	${(props) =>
-		props.right
-			? css`
-					right: 0.5rem;
-			  `
-			: css`
-					left: 0.5rem;
-			  `}
+  font-size: 2rem;
+  z-index: 10;
+  position: fixed;
+  top: 50%;
+  ${(props) =>
+    props.right
+      ? css`
+          right: 0.5rem;
+        `
+      : css`
+          left: 0.5rem;
+        `}
 `;
 
 function CardFlipPage() {
   const location = useLocation();
-  console.log('location:', location);
+  console.log("location:", location);
   const deck = location.state;
   const params = useParams();
 
   const [index, setIndex] = useState(0);
-	// const { loading, error, data } = useQuery(QUERY_CARD, {
-	// 	// variables: { deckId, cardId },
-	// 	variables: {
-	// 		deckId: '629fe32a513cfd52ed0f2d3f',
-	// 		cardId: '629fe32a513cfd52ed0f2d48',
-	// 	}, 
-	// }); // returns single card
-  const { loading, error, data } = useQuery(
-    QUERY_DECK, { variables: { deckId: params.id}}
-  )
+  // const { loading, error, data } = useQuery(QUERY_CARD, {
+  // 	// variables: { deckId, cardId },
+  // 	variables: {
+  // 		deckId: '629fe32a513cfd52ed0f2d3f',
+  // 		cardId: '629fe32a513cfd52ed0f2d48',
+  // 	},
+  // }); // returns single card
+  const { loading, error, data } = useQuery(QUERY_DECK, {
+    variables: { deckId: params.id },
+  });
 
-	if (loading) return <div>Loading</div>;
-	if (error) return <div>Error! {`${error.message}`}</div>;
-	// console.log('data', data);
-	// const card = data.card.cards;
-	// console.log('QUERY_CARD:', data.deck.cards);
+  if (loading) return <div>Loading</div>;
+  if (error) return <div>Error! {`${error.message}`}</div>;
+  // console.log('data', data);
+  // const card = data.card.cards;
+  // console.log('QUERY_CARD:', data.deck.cards);
   // console.log('params:', params);
   const card = data.deck.cards;
 
-	const setFinishedIndex = (i) => {
-		console.log('finished dragging on slide', i);
-		setIndex(i);
-	};
+  const setFinishedIndex = (i) => {
+    console.log("finished dragging on slide", i);
+    setIndex(i);
+  };
 
-	const next = () => {
-		if (index < card.length - 1) setIndex(index + 1);
-	};
+  const next = () => {
+    if (index < card.length - 1) setIndex(index + 1);
+  };
 
-	const previous = () => {
-		if (index > 0) setIndex(index - 1);
-	};
+  const previous = () => {
+    if (index > 0) setIndex(index - 1);
+  };
 
-	return (
-		<>
-			<GlobalStyles />
-			<AppStyles>
-				<Button onClick={previous} left disabled={index === 0}>
-					〈
-				</Button>
-				<Button onClick={next} right disabled={index === card.length - 1}>
-					〉
-				</Button>
-				<Slider
-					onSlideComplete={setFinishedIndex}
-					onSlideStart={(i) => {
-						console.clear();
-						console.log('started dragging on slide', i);
-					}}
-					activeIndex={index}
-					threshHold={100}
-					transition={0.5}
-					// scaleOnDrag={true}
-        >
-					{card.map((card, index) => (
-						<CardFlip card={card} key={index} />
-					))}
-				</Slider>
-			</AppStyles>
-		</>
-	);
+  return (
+    <>
+      <GlobalStyles />
+      <AppStyles>
+        <FlipWrapper>
+          <Button onClick={previous} left disabled={index === 0}>
+            〈
+          </Button>
+          <Button onClick={next} right disabled={index === card.length - 1}>
+            〉
+          </Button>
+          <Slider
+            onSlideComplete={setFinishedIndex}
+            onSlideStart={(i) => {
+              console.clear();
+              console.log("started dragging on slide", i);
+            }}
+            activeIndex={index}
+            threshHold={100}
+            transition={0.5}
+            // scaleOnDrag={true}
+          >
+            {card.map((card, index) => (
+              <CardFlip card={card} key={index} />
+            ))}
+          </Slider>
+        </FlipWrapper>
+      </AppStyles>
+    </>
+  );
 }
 
 export default CardFlipPage;
--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -7,13 +7,14 @@ import Category from "../components/ui/Category";
 
 import { useLazyQuery, useQuery } from "@apollo/client";
 import {
-  QUERY_DECKS, 
-  QUERY_DECKS_CATEGORY, 
-  QUERY_DECKS_TITLE
+  QUERY_DECKS,
+  QUERY_DECKS_CATEGORY,
+  QUERY_DECKS_TITLE,
 } from "../utils/queries";
 
 import classes from "./LandingPage.module.css";
-const slugify = require('slugify');
+import CardWrapper from "../components/ui/CardWrapper";
+const slugify = require("slugify");
 
 const LandingPage = () => {
   const [decks, setDecks] = useState("");
@@ -21,7 +22,7 @@ const LandingPage = () => {
 
   const [categories, setCategories] = useState("");
   const [queryCategory, {}] = useLazyQuery(QUERY_DECKS_CATEGORY);
-  
+
   const [search, setSearch] = useState("");
   const [queryTitle, {}] = useLazyQuery(QUERY_DECKS_TITLE);
 
@@ -37,22 +38,24 @@ const LandingPage = () => {
     event.preventDefault();
     // console.log('test', categories)
     if (categories.length && search !== "") {
-      const args = categories.map(category => category.value);
+      const args = categories.map((category) => category.value);
       // console.log(search, args);
-      const { data } = await queryDecks({ variables: { deckTitle: search, categories: args }});
+      const { data } = await queryDecks({
+        variables: { deckTitle: search, categories: args },
+      });
       // console.log("QUERY_DECKS:", data.decks);
       setDecks(data.decks);
       return;
     } else if (categories.length) {
-      const args = categories.map(category => category.value);
+      const args = categories.map((category) => category.value);
       // console.log(args);
-      const { data } = await queryCategory({ variables: { categories: args }});
+      const { data } = await queryCategory({ variables: { categories: args } });
       // console.log("QUERY_DECKS_CATEGORY:", data.decksCategory);
       setDecks(data.decksCategory);
       return;
     } else if (search !== "") {
       // console.log(search);
-      const { data } = await queryTitle({ variables: { deckTitle: search }});
+      const { data } = await queryTitle({ variables: { deckTitle: search } });
       // console.log("QUERY_DECKS_TITLE", data.decksTitle);
       setDecks(data.decksTitle);
       return;
@@ -63,40 +66,47 @@ const LandingPage = () => {
 
   return (
     <>
-      <Form onSubmit={handleSubmitSearch}>
+      <Form size="big" className={classes.form} onSubmit={handleSubmitSearch}>
         <div className={classes.search__container}>
-          <Category 
-            placeholder={'Select a Category'}
+          <Category
+            placeholder={"Select a Category"}
             handleChange={handleFormChange}
             categoryState={categories}
           />
-          <Form.Input 
-            placeholder={'Search Decks...'}
+        </div>
+        <div className={classes.search__container}>
+          <Form.Input
+            placeholder={"Search Decks..."}
             onChange={handleFormChange}
           />
         </div>
-        <Button type="submit">Search</Button>
+        <Button inverted color="teal" type="submit">
+          Search
+        </Button>
       </Form>
-      <Card.Group>
-        {decks.length ? (
-          decks.map((deck) => (
-            <Card 
-              as={Link} 
-              to={`/deck/${slugify(deck.title)}/${deck._id}`} 
-              state={deck} 
-              key={deck._id}
-            >
-              <Card.Content>{deck.title}</Card.Content>
-              <Card.Content>{deck.description}</Card.Content>
-              <Card.Content>
-                {deck.categories.map((category) => `${category.category} `)}
-              </Card.Content>
-            </Card>
-          ))
-        ) : (
-          <></>
-        )}
-      </Card.Group>
+      <CardWrapper>
+        <Card.Group>
+          {decks.length ? (
+            decks.map((deck) => (
+              <Card
+                color="blue"
+                as={Link}
+                to={`/deck/${slugify(deck.title)}/${deck._id}`}
+                state={deck}
+                key={deck._id}
+              >
+                <Card.Content>{deck.title}</Card.Content>
+                <Card.Content>{deck.description}</Card.Content>
+                <Card.Content>
+                  {deck.categories.map((category) => `${category.category} `)}
+                </Card.Content>
+              </Card>
+            ))
+          ) : (
+            <></>
+          )}
+        </Card.Group>
+      </CardWrapper>
     </>
   );
 };
