d66b5ef491d8137ab5db0cbe6fe79616dd2f99f4
yogs0ddhoth <blin7.webdev@gmail.com>
2022-06-15 00:59:22 -0400

    dependency issue

--- client/src/components/cart/Cart.js
+++ client/src/components/cart/Cart.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import { Link } from "react-router-dom";
 import { Card,  Header, Button } from "semantic-ui-react";
 import { loadStripe } from '@stripe/stripe-js';
 import { useLazyQuery, useMutation } from "@apollo/client";
