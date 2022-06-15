c026fc6b6ac9bdb1a48b9dacc5a3dd4984af34c9
Richard Murphy <richinatl@gmail.com>
2022-06-15 14:52:12 -0400

    fixed sizing issue on modal

--- client/src/components/quizmaster/Actions.js
+++ client/src/components/quizmaster/Actions.js
@@ -1,102 +1,98 @@
 import React, { useState } from "react";
-import { Modal, Icon, Button } from 'semantic-ui-react';
+import { Modal, Icon, Button } from "semantic-ui-react";
 
 import { DeckForm } from "./DeckForm";
 
-
 import Category from "../ui/Category";
 import CardForm from "./CardForm";
+import classes from "./Actions.module.css";
+import "../../index.css";
 
 const Action = ({ state, setState, icon, header, children }) => (
-  <Modal 
+  <Modal
+    id="modal"
+    className={classes.modal}
     onClose={() => setState(false)}
     onOpen={() => setState(true)}
     open={state}
     trigger={<Icon name={icon} />}
   >
     <Modal.Header>{header}</Modal.Header>
-    <Modal.Content>
-      {children}
-    </Modal.Content>
+    <Modal.Content>{children}</Modal.Content>
   </Modal>
 );
 
-export const EditDeck = ({ state, handleChange, handleSubmit}) => {
+export const EditDeck = ({ state, handleChange, handleSubmit }) => {
   const [editingDeck, setEditingDeck] = useState(false);
   return (
-    <Action 
-      state={editingDeck} 
-      setState={setEditingDeck} 
-      icon={'edit'} 
-      header={'Edit Deck Information:'}
+    <Action
+      state={editingDeck}
+      setState={setEditingDeck}
+      icon={"edit"}
+      header={"Edit Deck Information:"}
     >
-      <DeckForm 
+      <DeckForm
         handleChange={handleChange}
-        handleSubmit={
-          async (event) => {
-            await handleSubmit(event, 'UPDATE_DECK');
-            setEditingDeck(false);
-            window.location.reload();
-          }
-        }
+        handleSubmit={async (event) => {
+          await handleSubmit(event, "UPDATE_DECK");
+          setEditingDeck(false);
+          window.location.reload();
+        }}
         handleCancel={() => setEditingDeck(false)}
         state={state}
       >
         <Category
           placeholder={"Add a Category"}
           handleChange={handleChange}
-          categoryState={
-            state.categories
-          }
+          categoryState={state.categories}
         />
       </DeckForm>
     </Action>
-  )
+  );
 };
 
 export const EditCard = ({ state, card, handleChange, handleSubmit }) => {
   const [editingCard, setEditingCard] = useState(false);
   return (
-    <Action 
-      state={editingCard} 
-      setState={setEditingCard} 
-      icon={'edit'} 
-      header={'Edit Card:'}
+    <Action
+      state={editingCard}
+      setState={setEditingCard}
+      icon={"edit"}
+      header={"Edit Card:"}
     >
-      <CardForm 
+      <CardForm
         state={state}
         card={card}
         handleChange={handleChange}
-        handleSubmit={
-          async (event) => {
-            await handleSubmit(event, 'UPDATE_CARD', card._id);
-            setEditingCard(false);
-          }
-        }
+        handleSubmit={async (event) => {
+          await handleSubmit(event, "UPDATE_CARD", card._id);
+          setEditingCard(false);
+        }}
         handleCancel={() => setEditingCard(false)}
       />
     </Action>
-  )
-}
+  );
+};
 export const Delete = ({ header, action, stateId, handleDelete }) => {
   const [confirmDelete, setConfirmDelete] = useState(false);
 
   return (
-    <Action 
-      state={confirmDelete} 
-      setState={setConfirmDelete} 
-      icon={'trash alternate'} 
+    <Action
+      state={confirmDelete}
+      setState={setConfirmDelete}
+      icon={"trash alternate"}
       header={header}
     >
-      <Button 
-        onClick={ async () => {
-          console.log('test');
+      <Button
+        onClick={async () => {
+          console.log("test");
           await handleDelete(action, stateId);
           setConfirmDelete(false);
         }}
-      >Delete</Button>
+      >
+        Delete
+      </Button>
       <Button onClick={() => setConfirmDelete(false)}>Cancel</Button>
     </Action>
-
-  )
+  );
 };
